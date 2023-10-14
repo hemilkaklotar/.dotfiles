38f5a3ebdaacc28a5138320897d354b53a35e31c
Hemil Kaklotar <hemilkaklotar@gmail.com>
2023-10-15 01:34:10 +0530

    tmux config updated

--- dependency.sh
+++ dependency.sh
@@ -1,2 +1,2 @@
 sudo apt install gnome-tweaks picom alacritty tmux i3 zsh light pulseaudio pavucontrol -y
-sudo apt install gnome-shell-extension-manager blueman xrandr
+sudo apt install gnome-shell-extension-manager blueman xrandr htop neofetch xclip
