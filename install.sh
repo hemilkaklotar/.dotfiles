608c67934c20523ee15aaac5e843679d2d8ddbc7
Hemil Kaklotar <115414595+plater99@users.noreply.github.com>
2023-12-19 22:47:12 +0530

    restructured the folders and files , updated scripts to link configs

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 # link all the config to the os config
-source $HOME/.dotfiles/link.sh
+source $HOME/.dotfiles/scripts/link.sh
 
 # install all dependacies for the os
-source $HOME/.dotfiles/dependency.sh
+source $HOME/.dotfiles/scripts/dependency.sh
 
 # install all apps with flatpak
-source $HOME/.dotfiles/apps.sh
+source $HOME/.dotfiles/scripts/apps.sh
 
 # install the i3 wm with the necessary dependacies
-source $HOME/.dotfiles/i3-config.sh
+source $HOME/.dotfiles/scripts/i3-config.sh
