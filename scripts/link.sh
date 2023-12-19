df1327f5d8ddf00874e56412d4b18f88a3e3c054
Hemil Kaklotar <115414595+plater99@users.noreply.github.com>
2023-12-19 22:47:59 +0530

    removed the existing folders

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,27 +1,26 @@
 # remove existing file and folders
 rm -rf ~/.config/i3
-mkdir ~/.config/i3
-rm -rf ~/.config/i3/config
+#mkdir ~/.config/i3
 rm -f ~/.zshrc
 rm -f ~/.profile
 rm -rf ~/.config/picom
-mkdir ~/.config/picom
+#mkdir ~/.config/picom
 rm -rf ~/.config/alacritty
-mkdir ~/.config/alacritty
+#mkdir ~/.config/alacritty
 rm -rf ~/.config/i3status
-mkdir ~/.config/i3status
+#mkdir ~/.config/i3status
 rm -rf ~/.config/picom/picom.conf
 rm -f ~/.Xresources
 rm -f ~/.gitconfig
 rm -f ~/.xinitrc
 
-ln -s ~/.dotfiles/i3/config ~/.config/i3/config
+# sym link the config files to the user config
+ln -s ~/.dotfiles/configs/* ~/.config/
 ln -s ~/.dotfiles/.zshrc ~/
 ln -s ~/.dotfiles/.profile ~/
-# ln -s ~/.dotfiles/.bashrc ~/
-ln -s ~/.dotfiles/picom/picom.conf ~/.config/picom/picom.conf
-ln -s ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/
 ln -s ~/.dotfiles/.Xresources ~/.Xresources
 ln -s ~/.dotfiles/.gitconfig ~/
-ln -s  ~/.dotfiles/i3/i3status/config ~/.config/i3status/
 ln -s ~/.dotfiles/.xinitrc ~/
+#ln -s ~/.dotfiles/configs/picom/ ~/.config/
+#ln -s ~/.dotfiles/configs/alacritty/ ~/.config/
+#ln -s ~/.dotfiles/configs/i3/i3status/ ~/.config/
