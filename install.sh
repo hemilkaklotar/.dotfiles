0e3652e67a7fb9229a6da7335b5e2aa56de5f57e
Hemil Kaklotar <115414595+plater99@users.noreply.github.com>
2023-12-22 23:19:44 +0530

    config updated nvim installed

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-# link all the config to the os config
-source $HOME/.dotfiles/scripts/link.sh
-
 # install all dependacies for the os
 source $HOME/.dotfiles/scripts/dependency.sh
 
+# link all the config to the os config
+source $HOME/.dotfiles/scripts/link.sh
+
 # install all apps with flatpak
-source $HOME/.dotfiles/scripts/apps.sh
+#source $HOME/.dotfiles/scripts/apps.sh
 
 # install the i3 wm with the necessary dependacies
-source $HOME/.dotfiles/scripts/i3-config.sh
+#source $HOME/.dotfiles/scripts/i3-config.sh
--- scripts/link.sh
+++ scripts/link.sh
@@ -1,14 +1,10 @@
 # remove existing file and folders
 rm -rf ~/.config/i3
-#mkdir ~/.config/i3
 rm -f ~/.zshrc
 rm -f ~/.profile
 rm -rf ~/.config/picom
-#mkdir ~/.config/picom
 rm -rf ~/.config/alacritty
-#mkdir ~/.config/alacritty
 rm -rf ~/.config/i3status
-#mkdir ~/.config/i3status
 rm -rf ~/.config/picom/picom.conf
 rm -f ~/.Xresources
 rm -f ~/.gitconfig
@@ -21,6 +17,3 @@ ln -s ~/.dotfiles/.profile ~/
 ln -s ~/.dotfiles/.Xresources ~/.Xresources
 ln -s ~/.dotfiles/.gitconfig ~/
 ln -s ~/.dotfiles/.xinitrc ~/
-#ln -s ~/.dotfiles/configs/picom/ ~/.config/
-#ln -s ~/.dotfiles/configs/alacritty/ ~/.config/
-#ln -s ~/.dotfiles/configs/i3/i3status/ ~/.config/
