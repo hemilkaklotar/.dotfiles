7687e564a337aadfa73946f16bf339680a86c4d0
Hemil Kaklotar <115414595+plater99@users.noreply.github.com>
2023-12-26 22:53:51 +0530

    updated the modules and config

--- scripts/i3-config.sh
+++ scripts/i3-config.sh
@@ -1,11 +1,12 @@
 # install i3wm, alacritty and other required packages
-sudo apt install gnome-tweaks picom alacritty i3 light pulseaudio pavucontrol gnome-shell-extension-manager blueman xclip feh -y
+sudo apt install picom alacritty i3 pulseaudio pavucontrol blueman xclip feh -y
+sudo apt install polybar rofi light -y
 # to give permission run light without sudoers
 sudo chmod +s /usr/bin/light
-
+#sudo apt install gnome-tweaks gnome-shell-extension-manager -y
 # for gtk dark mode detection
-sudo apt install lxappearance gtk-chtheme -y
-sudo apt-get install gnome-sushi -y
+#sudo apt install lxappearance gtk-chtheme light -y
+#sudo apt-get install gnome-sushi -y
 
 #add policykit
-sudo apt-get install policykit-1-gnome -y
+#sudo apt-get install policykit-1-gnome -y
