271d4fa65c41106a29e361e0d13e688f05b6fb9b
plater99 <115414595+plater99@users.noreply.github.com>
2024-01-02 18:05:26 +0530

    updated package for policy agent

--- scripts/i3-config-fedora.sh
+++ scripts/i3-config-fedora.sh
@@ -4,12 +4,10 @@ sudo dnf install -y pavucontrol
 sudo dnf install -y blueman feh 
 sudo dnf install -y polybar rofi light
 # to give permission run light without sudoers
-# sudo chmod +s /usr/bin/light
-sudo dnf install -y gnome-tweaks 
-sudo dnf install -y gnome-shell-extension-manager 
+sudo chmod +s /usr/bin/light
 # for gtk dark mode detection
 sudo dnf install -y lxappearance qt5ct
 # sudo dnf install gnome-sushi gtk-chtheme -y
 sudo dnf install -y dunst xclip xdotool
-sudo dnf install -y policykit-1-gnome 
+sudo dnf install -y polkit-gnome 
 sudo dnf install -y mpd --allowerasing
