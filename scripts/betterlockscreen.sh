bf3c68084007d51a3f208f229d8f77381adfe58a
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-03-08 20:19:38 +0530

    updated config to use i3status instead of polybar

--- scripts/betterlockscreen.sh
+++ scripts/betterlockscreen.sh
@@ -21,28 +21,33 @@ if [[ ! -z $(which dnf) ]]; then
   sudo dnf install -y xdpyinfo
 elif [[ ! -z $(which apt) ]]; then
   sudo apt install -y autoconf
-  sudo apt install -y automake
-  sudo apt install -y cairo-devel
-  sudo apt install -y fontconfig
   sudo apt install -y gcc
-  sudo apt install -y libev-devel
-  sudo apt install -y libjpeg-turbo-devel
-  sudo apt install -y libXinerama
-  sudo apt install -y libxkbcommon-devel
-  sudo apt install -y libxkbcommon-x11-devel
-  sudo apt install -y libXrandr
-  sudo apt install -y pam-devel
-  sudo apt install -y pkgconf
-  sudo apt install -y xcb-util-image-devel
-  sudo apt install -y xcb-util-xrm-devel
-  sudo apt install -y xset
-  sudo apt install -y ImageMagick
-  sudo apt install -y xdpyinfo
+  sudo apt install -y make
+  sudo apt install -y pkg-config
+  sudo apt install -y libpam0g-dev
+  sudo apt install -y libcairo2-dev
+  sudo apt install -y libfontconfig1-dev
+  sudo apt install -y libxcb-composite0-dev
+  sudo apt install -y libev-dev 
+  sudo apt install -y libx11-xcb-dev
+  sudo apt install -y libxcb-xkb-dev
+  sudo apt install -y libxcb-xinerama0-dev
+  sudo apt install -y libxcb-randr0-dev
+  sudo apt install -y libxcb-image0-dev 
+  sudo apt install -y libxcb-util-dev
+  sudo apt install -y libxcb-xrm-dev
+  sudo apt install -y libxkbcommon-dev
+  sudo apt install -y libxkbcommon-x11-dev
+  sudo apt install -y libjpeg-dev
+  sudo apt install -y x11-xserver-utils
+  sudo apt install -y imagemagick
 fi
 
 git clone https://github.com/Raymo111/i3lock-color.git
 cd i3lock-color
 ./install-i3lock-color.sh
+cd ../
+rm -rf i3lock-color
 
 wget https://raw.githubusercontent.com/betterlockscreen/betterlockscreen/main/install.sh -O - -q | sudo bash -s system
 
