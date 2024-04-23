900945cef171dd349ae94b31d2712c6afba313cd
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-04-23 23:18:59 +0530

    updated zsh plugins

--- scripts/dev-setup/zsh-plugins.sh
+++ scripts/dev-setup/zsh-plugins.sh
@@ -2,12 +2,19 @@
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 if [[ ! -z $(which dnf) ]]; then
-  sudo dnf install -y eza zoxide
-  sudo dnf install -y ranger
-  sudo dnf install -y fzf
+	sudo dnf install -y eza zoxide
+	sudo dnf install -y ranger
+	sudo dnf install -y fzf
+	sudo dnf copr enable atim/lazygit -y
+	sudo dnf install lazygit -y
 elif [[ ! -z $(which apt-get) ]]; then
-  sudo apt  install -y ranger
-  sudo apt install -y zoxide
-  cargo install eza
-  sudo apt install -y fzf
+	sudo apt install -y ranger
+	sudo apt install -y zoxide
+	cargo install eza
+	sudo apt install -y fzf
+
+	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+	tar xf lazygit.tar.gz lazygit
+	sudo install lazygit /usr/local/bin
 fi
