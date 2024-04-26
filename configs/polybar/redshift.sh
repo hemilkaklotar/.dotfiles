97f4535802f4474792ee8b101f9b95d7a9e1620d
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-04-26 06:17:33 -0400

    removed color-scheme

---
+++ configs/polybar/redshift.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+
+checkIfRunning() {
+  if [ $(systemctl --user is-active redshift) == "active" ]; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+changeModeToggle() {
+
+  if checkIfRunning ; then
+    systemctl --user stop redshift
+  else
+    systemctl --user start redshift
+  fi
+}
+
+
+
+case $1 in 
+  toggle)
+  changeModeToggle
+    ;;
+  temperature)
+    if checkIfRunning ; then
+      CURRENT_TEMP=$(redshift -p 2> /dev/null  | grep "Color temperature" | sed 's/.*: //')
+      echo "$CURRENT_TEMP"
+    else
+      echo "off"
+    fi
+    ;;
+esac
