8a61d69d3b2777adbd07a3422f86ba79492f4630
Hemil Kaklotar <83568973+hemilkaklotar@users.noreply.github.com>
2024-05-18 09:37:00 +0000

    Updated zsh plugin script

--- scripts/zsh-plugins.sh
+++ scripts/zsh-plugins.sh
@@ -24,6 +24,7 @@ elif [[ ! -z $(which apt-get) ]]; then
 	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 	tar xf lazygit.tar.gz lazygit
 	sudo install lazygit /usr/local/bin
+  rm -rf lazygit lazygit.tar.gz
   sudo apt install -y bat
   mkdir -p ~/.local/bin
   ln -s /usr/bin/batcat ~/.local/bin/bat
